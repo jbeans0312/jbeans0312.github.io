37b10cf448a6633671f0821f873e29a6e2618331
jbeans0312 <jbeans@udel.edu>
2023-09-05 12:04:53 -0400

    added resume, updated fun facts

--- js/fact.js
+++ js/fact.js
@@ -1,13 +1,8 @@
 function showQuote(){
     var facts=[
-        'When the full moon rises, I turn into a spider.',
-        'My summoning circle is BlueTooth compatible.',
-        'My face is green and serpentine.',
-        'I conclude!',
-        'My favorite tea is chamomile.',
-        'M0re 2 come',
-        'There are always rooms in the Magpie\'s Knapsack.',
-        'HARK!'
+        'I grew up in Connecticut',
+        'Long sandwiches are called grinders, not hoagies',
+        'My top strength is Harmony',
     ]
     var pick=Math.floor(Math.random() * (facts.length));
     document.getElementById('fact').innerHTML = 'Fun Fact: ' + facts[pick]
